62e849fff0f8bcb7eaeab3482f516cdf63939d1c
Gary Tou <gary@garytou.com>
2022-01-07 13:57:32 -0800

    WIP: Worksheet 2

---
+++ worksheet2.py
@@ -0,0 +1,43 @@
+"""
+Gary Tou
+CPSC 3400, Worksheet 2
+1/7/2022
+"""
+
+
+def worksheet2a():
+    # Gary: Sharer
+    # Kate: Narrator
+    # Terence: Reporter
+    lo = int(input('low: '))
+    hi = int(input('high: '))
+    for i in range(lo, hi, 3):
+        print(i)
+
+
+def worksheet2b():
+    # Gary: Narrator
+    # Kate: Reporter
+    # Terence: Sharer
+    pass
+
+
+def worksheet2c():
+    # Individual work
+    pass
+
+
+def worksheet2d():
+    # Individual work
+    pass
+
+
+def main():
+    worksheet2a()
+    # worksheet2b()
+    # worksheet2c()
+    # worksheet2d()
+
+
+if __name__ == "__main__":
+    main()
