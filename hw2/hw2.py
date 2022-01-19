3bc31a2c214fa7fca3d747fc2409d1ec58455030
Gary Tou <gary@garytou.com>
2022-01-19 13:07:58 -0800

    setup hw2 file

---
+++ hw2/hw2.py
@@ -0,0 +1,28 @@
+"""
+Gary Tou
+CPSC 3400, HW 2
+1/19/2022
+https://seattleu.instructure.com/courses/1602042/assignments/6989792
+"""
+
+import sys
+
+# These need to be at global scope
+class ImproperTimeError(Exception):
+
+class EmtpyFileError(Exception):
+
+def create_time_list(filename):
+    
+def time_compare_gen(time_list, target):
+
+
+def future_time(a, b):
+    
+def minutes_from_midnight(tt):
+
+
+def main(filename):
+    
+
+if __name__ == '__main__':
