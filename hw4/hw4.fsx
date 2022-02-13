010768153a465ccc85d20e398c3b00f8003cbe60
Gary Tou <gary@garytou.com>
2022-02-13 06:13:03 -0800

    hw4 without print

--- hw4/hw4.fsx
+++ hw4/hw4.fsx
@@ -32,16 +32,6 @@ let volume dimension =
 let maxCylinderVolume dimensions =
     max (map (dimensions, volume), (fun a b -> if a > b then a else b))
 
-printfn
-    "%A"
-    (maxCylinderVolume [ (2.1, 3.4)
-                         (4.7, 2.8)
-                         (0.9, 6.1)
-                         (3.2, 5.4) ])
-
-printfn "%A" (maxCylinderVolume [ (0.33, 0.66) ])
-
-
 
 // Write a function elimDuplicates that takes a list of integers and eliminates
 // consecutive duplicates; replacing them with a single instance of the value.
@@ -58,46 +48,6 @@ let rec elimDuplicates (values: list<int>) =
         | hd' :: tl' when hd = hd' -> hd :: elimDuplicates tl' // Removes duplicate
         | hd' :: tl' -> hd :: hd' :: elimDuplicates tl'
 
-printfn
-    "%A"
-    (elimDuplicates [ 1
-                      2
-                      2
-                      3
-                      3
-                      3
-                      4
-                      4
-                      4
-                      4
-                      5
-                      5
-                      5
-                      5
-                      5 ])
-
-printfn
-    "%A"
-    (elimDuplicates [ 1
-                      2
-                      2
-                      1
-                      3
-                      3
-                      1
-                      4
-                      4
-                      1
-                      5
-                      5
-                      1 ])
-
-
-
-
-
-
-// BST
 
 // Tree definition for problem 3
 type BST =
@@ -144,20 +94,3 @@ let rec count (func: (int -> bool), tree: BST) =
 // Returns the number of nodes that contain even integers. REQUIREMENT: This
 // function must be a single call to `count` (part 3C) using a lambda function.
 let evenCount (tree: BST) = count ((fun x -> x % 2 = 0), tree)
-
-
-
-let bt1 = insert (10, Empty)
-printfn "%A" bt1
-let bt2 = insert (5, bt1)
-printfn "%A" bt2
-let bt3 = insert (3, bt2)
-printfn "%A" bt3
-let bt4 = insert (17, bt3)
-printfn "%A" bt4
-let bt5 = insert (12, bt4)
-printfn "%A" bt5
-
-printfn "%A" (search (17, bt5))
-printfn "%A" (search (4, bt5))
-printfn "%A" (evenCount bt5)
